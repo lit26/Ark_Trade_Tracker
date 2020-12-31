6746e54d01c716965ae6772e79f90447af585884
Tianning Li <ltianningli@gmail.com>
2020-12-30 19:08:31 -0800

    update chart

--- src/components/Portfoliochart.js
+++ src/components/Portfoliochart.js
@@ -4,17 +4,22 @@ import Plot from 'react-plotly.js';
 function Portfoliochart({data}) {
     const [market, setMarket] = useState([]);
     const [text, setText] = useState([]);
+    const [hoverText, setHoverText] = useState([]);
 
     useEffect(() =>  {
         let market_text = [];
         let market_value = [];
+        let hover_text = [];
         data.slice(0,10).map(value =>{
             market_text.push(value[0]);
             market_value.push(value[1]);
+            hover_text.push(`${value[0]}<br>` +
+            `${value[1].toLocaleString('en-US',{style: 'currency', currency: 'USD'})}<br>`)
             return '';
         })
         setText(market_text);
-        setMarket(market_value)
+        setMarket(market_value);
+        setHoverText(hover_text);
     },[data])
 
     return (
@@ -23,6 +28,9 @@ function Portfoliochart({data}) {
                 type: 'pie',
                 values: market,
                 labels: text,
+                textinfo:'percent',
+                hoverinfo: 'text',
+                text: hoverText,
                 name: "Portfolio",
                 hole: 0.5,
                 showlegend:false,
